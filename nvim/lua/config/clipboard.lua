7238bf5cf3527c8e783052482577696e678f7a6b
tom <>
2023-04-21 15:18:38 -0600

    clipboard

--- nvim/lua/config/clipboard.lua
+++ nvim/lua/config/clipboard.lua
@@ -1,15 +1,20 @@
 vim.opt.clipboard = { 'unnamed', 'unnamedplus' }
 
-if vim.env.SSH_CONNECTION
-    or vim.endswith(vim.env.WEZTERM_EXECUTABLE or '', 'wezterm-mux-server') then
-  local function copy(str)
+vim.cmd.runtime('autoload/provider/clipboard.vim')
+
+if vim.g.loaded_clipboard_provider == 1 then
+  vim.g.loaded_clipboard_provider = nil
+
+  local function copy(lines, _)
     vim.cmd.wshada()
-    vim.fn.chansend(vim.v.stderr, vim.fn.system('clip', str))
+    vim.loop.new_tty(1, false):write(
+      string.format('\027]52;c;%s\027\\',
+        vim.fn.system("base64 | tr -d '\\n\\r'", table.concat(lines, '\n'))))
   end
 
   local function paste()
     vim.cmd.rshada()
-    return { vim.fn.getreg('', 1, true), 'V' }
+    return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
   end
 
   vim.g.clipboard = {
@@ -23,4 +28,6 @@ if vim.env.SSH_CONNECTION
       ['+'] = paste,
     }
   }
+
+  vim.cmd.runtime('autoload/provider/clipboard.vim')
 end
