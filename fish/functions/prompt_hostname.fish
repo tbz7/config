5c4d6f3ea70a551857d70af53402a583b38819ce
tom <>
2024-11-05 10:40:56 -0700

    shpool session naming

--- fish/functions/prompt_hostname.fish
+++
@@ -1,7 +0,0 @@
-function prompt_hostname --description 'short hostname for the prompt'
-    if string match -q '*-shpool-*' "$SHPOOL_SESSION_NAME"
-        string replace -- -shpool "" $SHPOOL_SESSION_NAME
-    else
-        string replace -r -- '\..*' '' $hostname
-    end
-end
