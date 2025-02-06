009245f5dff2b297dbf0748b80da1e778cf8b469
tom <tom@>
2025-02-06 15:50:14 -0700

    Use static themes

---
+++ fish/conf.d/theme.fish
@@ -0,0 +1,31 @@
+status is-interactive || return
+
+set -g fish_color_autosuggestion brblack --dim
+set -g fish_color_cancel brblack
+set -g fish_color_command brblue
+set -g fish_color_comment brblack --italics
+set -g fish_color_cwd blue
+set -g fish_color_cwd_root red
+set -g fish_color_end cyan
+set -g fish_color_error brred
+set -g fish_color_escape yellow
+set -g fish_color_history_current --bold
+set -g fish_color_host brgreen
+set -g fish_color_host_remote yellow
+set -g fish_color_keyword magenta
+set -g fish_color_normal normal
+set -g fish_color_operator cyan
+set -g fish_color_option yellow
+set -g fish_color_param normal
+set -g fish_color_quote brgreen
+set -g fish_color_redirection cyan
+set -g fish_color_search_match --background=brblack
+set -g fish_color_selection --background=brblack
+set -g fish_color_status red
+set -g fish_color_user yellow
+set -g fish_color_valid_path --underline
+set -g fish_pager_color_completion
+set -g fish_pager_color_description cyan
+set -g fish_pager_color_prefix --bold
+set -g fish_pager_color_progress cyan
+set -g fish_pager_color_selected_background --background=brblack
